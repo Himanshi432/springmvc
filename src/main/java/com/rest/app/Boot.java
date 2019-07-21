bb783153805d6c8cf89e5e432c6627031a4edaa2
himanshi <himanshi.singh2411@gmail.com>
2019-07-21 12:41:39 +0530

    Swagger integration with service

--- src/main/java/com/rest/app/Boot.java
+++ src/main/java/com/rest/app/Boot.java
@@ -1,12 +1,16 @@
 package com.rest.app;
 
+import com.rest.config.Swagger2Config;
 import com.rest.controller.HealthController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = HealthController.class)
+@ComponentScan(basePackageClasses = Swagger2Config.class)
+@EnableSwagger2
 public class Boot {
     public static void main(String args[]) {
         SpringApplication.run(Boot.class, args);
