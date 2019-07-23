d8c5169299d5997e6e9ed23cc94f7f4551f4b701
himanshi <himanshi.singh2411@gmail.com>
2019-07-24 00:42:57 +0530

    Address PUT/POST/DELETE impl

--- src/main/java/com/rest/controller/AddressController.java
+++ src/main/java/com/rest/controller/AddressController.java
@@ -1,12 +1,11 @@
 package com.rest.controller;
 
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import com.rest.dao.Address;
 import com.rest.manager.AddressManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AddressController {
@@ -17,5 +16,21 @@ public class AddressController {
     @RequestMapping(method = RequestMethod.GET,value= "/address")
     public Address getAddress(@RequestParam(value = "address_id",defaultValue ="NOIDA") String address_id){
         return addressManager.getAddressDetails(address_id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value= "/address")
+    public String addNewAddress(@RequestBody Address address){
+        return addressManager.addNewAddress(address);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value= "/address")
+    public UpdateResult updateAddress(@RequestParam(value = "address_id",defaultValue ="NOIDA") String address_id,
+                                      @RequestBody Address address){
+        return addressManager.updateAddress(address_id,address);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value="/address")
+    public DeleteResult deleteAddress(@RequestParam(value = "address_id",defaultValue ="NOIDA") String address_id){
+        return addressManager.deleteAddress(address_id);
     }
 }
--- src/main/java/com/rest/manager/AddressManager.java
+++ src/main/java/com/rest/manager/AddressManager.java
@@ -1,9 +1,13 @@
 package com.rest.manager;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.util.JSON;
 import com.rest.clients.MongoDBClient;
 import com.rest.dao.Address;
 import com.rest.utils.TPCConstants;
@@ -21,13 +25,13 @@ public class AddressManager {
 
     @Autowired
     ObjectMapper objectMapper;
+
     public Address getAddressDetails(String address_id){
         MongoCollection<Document> mongoCollection = mongoDBClient
                 .buildMongoCollection(TPCConstants.DB_STRING,TPCConstants.MONGO_ADDRESS_COLLECTION,TPCConstants.DB_NAME);
         BasicDBObject getQuery = new BasicDBObject();
         getQuery.put("address_id",address_id);
         MongoCursor<Document> mongoCursor = mongoDBClient.find(mongoCollection,getQuery);
-        System.out.println("Addres_id=" + address_id);
         Document result = mongoCursor.next();
         Address foundaddress = null;
         try {
@@ -36,5 +40,34 @@ public class AddressManager {
             e.printStackTrace();
         }
         return foundaddress;
+    }
+
+    public String addNewAddress(Address newaddressbody){
+        MongoCollection<Document> mongoCollection = mongoDBClient
+                .buildMongoCollection(TPCConstants.DB_STRING,TPCConstants.MONGO_ADDRESS_COLLECTION,TPCConstants.DB_NAME);
+        Gson gson = new Gson();
+        BasicDBObject dbDocument = (BasicDBObject) JSON.parse(gson.toJson(newaddressbody));
+        mongoDBClient.insert(mongoCollection,new Document(dbDocument.toMap()));
+        return "New address added";
+    }
+
+    public UpdateResult updateAddress(String address_id, Address addressbody){
+        MongoCollection<Document> mongoCollection = mongoDBClient
+                .buildMongoCollection(TPCConstants.DB_STRING,TPCConstants.MONGO_ADDRESS_COLLECTION,TPCConstants.DB_NAME);
+        Gson gson = new Gson();
+        BasicDBObject dbDocument = (BasicDBObject) JSON.parse(gson.toJson(addressbody));
+        Document querydocument = new Document();
+        querydocument.put("address_id",address_id);
+        return mongoDBClient.update(mongoCollection,querydocument,new Document(dbDocument.toMap()));
+
+    }
+
+    public DeleteResult deleteAddress(String address_id){
+        MongoCollection<Document> mongoCollection = mongoDBClient
+                .buildMongoCollection(TPCConstants.DB_STRING,TPCConstants.MONGO_ADDRESS_COLLECTION,TPCConstants.DB_NAME);
+        Document querydocument = new Document();
+        querydocument.put("address_id",address_id);
+        return mongoDBClient.delete(mongoCollection,querydocument);
+
     }
 }
