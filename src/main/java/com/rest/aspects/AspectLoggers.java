481e73afa2c1dcb5c501c8e4b35e8a2c7c98ecca
himanshi <himanshi.singh2411@gmail.com>
2019-08-11 17:30:13 +0530

    Added aspects for all the methods of all the classes called in application.

--- src/main/java/com/rest/aspects/AspectLoggers.java
+++ src/main/java/com/rest/aspects/AspectLoggers.java
@@ -19,4 +19,56 @@ public class AspectLoggers {
     public void afterMethodInvocation(JoinPoint joinPoint){
         System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
     }
+
+    @Before("execution(* com.rest.controller.*.*(..))")
+    public void beforeMethodInvocationcontroller(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " is called for execution.");
+    }
+
+    @After("execution(* com.rest.controller.*.*(..))")
+    public void afterMethodInvocationcontroller(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
+    }
+
+    @Before("execution(* com.rest.clients.*.*(..))")
+    public void beforeMethodInvocationMongo(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " is called for execution.");
+    }
+
+    @After("execution(* com.rest.clients.*.*(..))")
+    public void afterMethodInvocationMongo(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
+    }
+
+    @Before("execution(* com.rest.exceptions.*.*(..))")
+    public void beforeMethodInvocationExceptions(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " is called for execution.");
+    }
+
+    @After("execution(* com.rest.exceptions.*.*(..))")
+    public void afterMethodInvocationExceptions(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
+    }
+
+
+    @Before("execution(* com.rest.dao.*.*(..))")
+    public void beforeMethodInvocationDAO(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " is called for execution.");
+    }
+
+    @After("execution(* com.rest.dao.*.*(..))")
+    public void afterMethodInvocationDAO(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
+    }
+
+    @Before("execution(* com.rest.utils.*.*(..))")
+    public void beforeMethodInvocationTPCConstants(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " is called for execution.");
+    }
+
+    @After("execution(* com.rest.utils.*.*(..))")
+    public void afterMethodInvocationTPCConstants(JoinPoint joinPoint){
+        System.out.println("Method " + joinPoint.getSignature().getName() + " has finished execution.");
+    }
+
 }
